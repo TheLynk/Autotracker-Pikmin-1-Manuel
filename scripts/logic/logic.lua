4efe73de5b9993bc0a562eded8ab5cc3c88bcb69
TheLynk <jeanpol4226@gmail.com>
2025-07-14 22:31:52 +0200

    Starting Forest of Hope

--- scripts/logic/logic.lua
+++ scripts/logic/logic.lua
@@ -1,1 +1,27 @@
 ScriptHost:LoadScript("scripts/autotracking/archipelago.lua")
+
+function has(item, amount)
+    local count = Tracker:ProviderCountForCode(item)
+    if not amount then
+        return count > 0
+    else
+        amount = tonumber(amount)
+        return count >= amount
+    end
+end
+
+function hasnot(item)
+    local count2 = Tracker:ProviderCountForCode(item)
+    if count2 > 0 then
+        return false
+    else return true
+    end
+end
+
+function skip_box_FoH()
+    if has("final_trial") then
+        return AccessibilityLevel.SequenceBreak
+    else
+        return false
+    end
+end
