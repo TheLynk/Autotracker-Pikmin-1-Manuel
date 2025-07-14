4501538e0c7f8662eba299537c1f7c18a0be71d2
TheLynk <jeanpol4226@gmail.com>
2025-07-14 20:40:27 +0200

    update Autotratracking

--- scripts/autotracking/location_mapping.lua
+++ scripts/autotracking/location_mapping.lua
@@ -5,16 +5,16 @@ LOCATION_MAPPING = {
 
 [2] = {"@Crash Site/Box/Push the Box"},
 
-[3] = {"@Crash Site/Ship Part 1/Main Engine"},
-[7] = {"@Crash Site/Ship Part 2/Positron Generator"},
+[3] = {"@Crash Site/Main Engine/Ship Part"},
+[7] = {"@Crash Site/Positron Generator/Ship Part"},
 
 [72] = {"@Crash Site/Spawn/Crash on PNF-404"},
 [4] = {"@Crash Site/Spawn/Tutorial Complete! (Sunset)"},
 [73] = {"@Crash Site/Spawn/Re Explore"},
 [66] = {"@Crash Site/Spawn/100%"},
 
-[70] = {"@Crash Site/Wall 1/Blast White Wall"},
-[71] = {"@Crash Site/Wall 2/Break White Wall"},
+[70] = {"@Crash Site/Blast White Wall/Wall"},
+[71] = {"@Crash Site/Break White Wall/Wall"},
 
 -- The Forest of Hope
 
